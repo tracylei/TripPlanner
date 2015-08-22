4fe2510bcdcaac156a1f0a25abdb86b9cda7de54
Tracy Lei <yqtlei@uwaterloo.ca>
2015-08-22 17:10:42 -0400

    initial commit

---
+++ app/src/main/java/com/example/android/tripplanner/DisplayTrips.java
@@ -0,0 +1,7 @@
+package com.example.android.tripplanner;
+
+/**
+ * Created by tracylei on 2015-08-22.
+ */
+public class DisplayTrips {
+}
---
+++ app/src/main/java/com/example/android/tripplanner/PlanTrip.java
@@ -0,0 +1,16 @@
+package com.example.android.tripplanner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tracylei on 2015-08-22.
+ */
+public class PlanTrip extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trip_plan);
+    }
+
+}
