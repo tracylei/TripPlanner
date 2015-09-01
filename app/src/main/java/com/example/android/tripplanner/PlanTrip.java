fa5a77649af67647e38942c749c076ded4e6a601
Tracy Lei <yqtlei@uwaterloo.ca>
2015-08-31 23:03:50 -0400

    change option 2 screen to fram frame layout

--- app/src/main/java/com/example/android/tripplanner/PlanTrip.java
+++ app/src/main/java/com/example/android/tripplanner/PlanTrip.java
@@ -7,6 +7,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 
 /**
@@ -17,10 +19,14 @@ import android.view.View;
 public class PlanTrip extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private int destCount;
+    private LinearLayout screen1;
+    private RelativeLayout screen2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.trip_plan);
+        setContentView(R.layout.option2_plan_trip);
 
         PlaceAutocompleteFragment paFragment = new PlaceAutocompleteFragment();
         DestinationAutocompleteFragment daFragment = new DestinationAutocompleteFragment();
@@ -32,15 +38,27 @@ public class PlanTrip extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+        destCount=1;
     }
 
 
-    //Button click on fragment
+    //Plus button click on fragment
     public void addDest(View v) {
-        DestinationAutocompleteFragment daFragment = new DestinationAutocompleteFragment();
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(R.id.dest1_holder, daFragment, "Destination 1 autocomplete box");
-        transaction.commit();
+        if (destCount < 6) {
+            DestinationAutocompleteFragment daFragment = new DestinationAutocompleteFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.add(R.id.dest1_holder, daFragment, "Destination 1 autocomplete box");
+            transaction.commit();
+            destCount++;
+        }
+    }
+
+    //Next button click on screen 1
+    public void firstNext(View v){
+        screen1 = (LinearLayout) findViewById(R.id.screen1);
+        screen2 = (RelativeLayout) findViewById(R.id.screen2);
+        screen1.setVisibility(View.GONE);
+        screen2.setVisibility(View.VISIBLE);
     }
 }
